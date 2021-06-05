ddb83c8617ac015479b53b0d25fe0963f88141f3
yashreddyk <32071335+yashreddyk@users.noreply.github.com>
2021-06-05 19:05:58 -0400

    Update answer.py

--- answer.py
+++ answer.py
@@ -6,13 +6,9 @@ from transformers.pipelines import pipeline
 from flask import Flask
 from flask import request, jsonify
 
-
-#--------------#
-#  VARIABLES   #
-#--------------#
-
-# Create my flask app
-app = Flask(__name__)
+#---------------------#
+#  Create Flask App   #
+#---------------------#
 
 # Create a variable that will hold our models in memory
 models = {}
@@ -20,252 +16,267 @@ models = {}
 # The database file
 db = 'answers.db'
 
-#--------------#
-#    ROUTES    #
-#--------------#
-
-# Define a handler for the / path, which
-# returns a message and allows Cloud Run to 
-# health check the API.
-@app.route("/")
-def hello_world():
-    return "<p>The question answering API is healthy!</p>"
 
 
-# Define a handler for the /answer path, which
-# processes a JSON payload with a question and
-# context and returns an answer using a Hugging
-# Face model.
-@app.route("/answer", methods=['POST'])
-def answer():
+def create_app():
 
-    # Get the request body data
-    data = request.json
+    # Create my flask app
+    app = Flask(__name__)
 
-    # Validate model name if given
-    if request.args.get('model') != None:
-        if not validate_model(request.args.get('model')):
-            return "Model not found", 400
+    # Create a variable that will hold our models in memory
+    #models = {}
 
-    # Answer the question
-    answer, model_name = answer_question(request.args.get('model'), 
-            data['question'], data['context'])
-    timestamp = int(time.time())
+    # The database file
+    #db = 'answers.db'
 
-    # Insert our answer in the database
+    #--------------#
+    #    ROUTES    #
+    #--------------#
+
+    # Define a handler for the / path, which
+    # returns a message and allows Cloud Run to 
+    # health check the API.
+    @app.route("/")
+    def hello_world():
+        return "<p>The question answering API is healthy!</p>"
+
+
+    # Define a handler for the /answer path, which
+    # processes a JSON payload with a question and
+    # context and returns an answer using a Hugging
+    # Face model.
+    @app.route("/answer", methods=['POST'])
+    def answer():
+
+        # Get the request body data
+        data = request.json
+
+        # Validate model name if given
+        if request.args.get('model') != None:
+            if not validate_model(request.args.get('model')):
+                return "Model not found", 400
+
+        # Answer the question
+        answer, model_name = answer_question(request.args.get('model'), 
+                data['question'], data['context'])
+        timestamp = int(time.time())
+
+        # Insert our answer in the database
+        # con = sqlite3.connect(db)
+        # Insert our answer in the database
     # con = sqlite3.connect(db)
-    rootcert_content = format(os.environ.get('SERVER_CA'))
-    fcert_content = format(os.environ.get('CLIENT_CERT'))
-    fkey_content = format(os.environ.get('CLIENT_KEY'))
-      
-    #Write SSL content into temp files
-    frootcert = open("sslrootcert.txt","w")
-    frootcert.write(rootcert_content.replace("@","="))
-    frootcert.close()
-    fcert = open("sslcert.txt","w")
-    fcert.write(fcert_content.replace("@","="))
-    fcert.close()
-    fkey = open("sslkey.txt","w")
-    fkey.write(fkey_content.replace("@","="))
-    fkey.close()
-
-    #Change unix permissions to restricted
-    os.chmod("sslrootcert.txt",0o600)
-    os.chmod("sslcert.txt",0o600)
-    os.chmod("sslkey.txt",0o600)
-
-    sslmode="sslmode=verify-ca"
-    sslrootcert = "sslrootcert=sslrootcert.txt"
-    sslcert = "sslcert=sslcert.txt"
-    sslkey = "sslkey=sslkey.txt"
-    hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
-    user = "user=postgres"
-    password = "password={}".format(os.environ.get('PG_PASSWORD'))
-    dbname="dbname=mgmt590"
-
-    dbconnect = " ".join([sslmode,
-                            sslrootcert,
-                            sslcert,
-                            sslkey,
-                            hostaddr,
-                            user,
-                            dbname,password])
-    con = psycopg2.connect(dbconnect)    
-    cur = con.cursor()
-    sql = "INSERT INTO answers VALUES ('{question}','{context}','{model}','{answer}',{timestamp})"
-    cur.execute(sql.format(
-        question=data['question'].replace("'", "''"), 
-        context=data['context'].replace("'", "''"), 
-        model=model_name, 
-        answer=answer, 
-        timestamp=str(timestamp)))
-    con.commit()
-    con.close()
-
-    # Create the response body.
-    out = {
-        "question": data['question'],
-        "context": data['context'],
-        "answer": answer,
-        "model": model_name,
-        "timestamp": timestamp
-    }
-
-    return jsonify(out)
-
-
-# List historical answers from the database.
-@app.route("/answer", methods=['GET'])
-def list_answer():
-
-    # Validate timestamps
-    if request.args.get('start') == None or request.args.get('end') == None:
-        return "Query timestamps not provided", 400
-
-    # Prep SQL query
-    if request.args.get('model') != None:
-        sql = "SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end} AND model == '{model}'"
-        sql_rev = sql.format(start=request.args.get('start'), 
-                end=request.args.get('end'), model=request.args.get('model'))
-    else:
-        sql = 'SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end}'
-        sql_rev = sql.format(start=request.args.get('start'), end=request.args.get('end'))
-
-    # Query the database
-    # con = sqlite3.connect(db)
-    rootcert_content = format(os.environ.get('SERVER_CA'))
-    fcert_content = format(os.environ.get('CLIENT_CERT'))
-    fkey_content = format(os.environ.get('CLIENT_KEY'))
-      
-    #Write SSL content into temp files
-    frootcert = open("sslrootcert.txt","w")
-    frootcert.write(rootcert_content.replace("@","="))
-    frootcert.close()
-    fcert = open("sslcert.txt","w")
-    fcert.write(fcert_content.replace("@","="))
-    fcert.close()
-    fkey = open("sslkey.txt","w")
-    fkey.write(fkey_content.replace("@","="))
-    fkey.close()
-
-    #Change unix permissions to restricted
-    os.chmod("sslrootcert.txt",0o600)
-    os.chmod("sslcert.txt",0o600)
-    os.chmod("sslkey.txt",0o600)
-
-    sslmode="sslmode=verify-ca"
-    sslrootcert = "sslrootcert=sslrootcert.txt"
-    sslcert = "sslcert=sslcert.txt"
-    sslkey = "sslkey=sslkey.txt"
-    hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
-    user = "user=postgres"
-    password = "password={}".format(os.environ.get('PG_PASSWORD'))
-    dbname="dbname=mgmt590-database"
-
-    dbconnect = " ".join([sslmode,
-                            sslrootcert,
-                            sslcert,
-                            sslkey,
-                            hostaddr,
-                            user,
-                            dbname,password])
-    con = psycopg2.connect(dbconnect)
-    cur = con.cursor()
-    out = []
-    for row in cur.execute(sql_rev):
-        out.append({
-            "question": row[0],
-            "context": row[1],
-            "answer": row[2],
-            "model": row[3],
-            "timestamp": row[4]
-        })
-    con.close()
-
-    return jsonify(out)
-
-
-# List models currently available for inference
-@app.route("/models", methods=['GET'])
-def list_model():
-
-    # Get the loaded models
-    models_loaded = []
-    for m in models['models']:
-        models_loaded.append({
-            'name': m['name'],
-            'tokenizer': m['tokenizer'],
-            'model': m['model']
-        })
-
-    return jsonify(models_loaded)
-
-
-# Add a model to the models available for inference
-@app.route("/models", methods=['PUT'])
-def add_model():
-
-    # Get the request body data
-    data = request.json
+        rootcert_content = format(os.environ.get('PG_SSLROOTCERT'))
+        fcert_content = format(os.environ.get('PG_SSLCERT'))
+        fkey_content = format(os.environ.get('PG_SSLKEY'))
+          
+        #Write SSL content into temp files
+        frootcert = open("sslrootcert.txt","w")
+        frootcert.write(rootcert_content.replace("@","="))
+        frootcert.close()
+        fcert = open("sslcert.txt","w")
+        fcert.write(fcert_content.replace("@","="))
+        fcert.close()
+        fkey = open("sslkey.txt","w")
+        fkey.write(fkey_content.replace("@","="))
+        fkey.close()
     
-    # Load the provided model
-    if not validate_model(data['name']):
+        #Change unix permissions to restricted
+        os.chmod("sslrootcert.txt",0o600)
+        os.chmod("sslcert.txt",0o600)
+        os.chmod("sslkey.txt",0o600)
+    
+        sslmode="sslmode=verify-ca"
+        sslrootcert = "sslrootcert=sslrootcert.txt"
+        sslcert = "sslcert=sslcert.txt"
+        sslkey = "sslkey=sslkey.txt"
+        hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
+        user = "user=postgres"
+        password = "password={}".format(os.environ.get('PG_PASSWORD'))
+        dbname="dbname=mgmt590"
+    
+        dbconnect = " ".join([sslmode,
+                                sslrootcert,
+                                sslcert,
+                                sslkey,
+                                hostaddr,
+                                user,
+                                dbname,password])
+        con = psycopg2.connect(dbconnect)
+        cur = con.cursor()
+        sql = "INSERT INTO answers VALUES ('{question}','{context}','{model}','{answer}',{timestamp})"
+        cur.execute(sql.format(
+            question=data['question'].replace("'", "''"), 
+            context=data['context'].replace("'", "''"), 
+            model=model_name, 
+            answer=answer, 
+            timestamp=str(timestamp)))
+        con.commit()
+        con.close()
+
+        # Create the response body.
+        out = {
+            "question": data['question'],
+            "context": data['context'],
+            "answer": answer,
+            "model": model_name,
+            "timestamp": timestamp
+        }
+
+        return jsonify(out)
+
+
+    # List historical answers from the database.
+    @app.route("/answer", methods=['GET'])
+    def list_answer():
+
+        # Validate timestamps
+        if request.args.get('start') == None or request.args.get('end') == None:
+            return "Query timestamps not provided", 400
+
+        # Prep SQL query
+        if request.args.get('model') != None:
+            sql = "SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end} AND model == '{model}'"
+            sql_rev = sql.format(start=request.args.get('start'), 
+                    end=request.args.get('end'), model=request.args.get('model'))
+        else:
+            sql = 'SELECT * FROM answers WHERE timestamp >= {start} AND timestamp <= {end}'
+            sql_rev = sql.format(start=request.args.get('start'), end=request.args.get('end'))
+
+        # Query the database
+        # con = sqlite3.connect(db)
+        rootcert_content = format(os.environ.get('PG_SSLROOTCERT'))
+        fcert_content = format(os.environ.get('PG_SSLCERT'))
+        fkey_content = format(os.environ.get('PG_SSLKEY'))
+          
+        #Write SSL content into temp files
+        frootcert = open("sslrootcert.txt","w")
+        frootcert.write(rootcert_content.replace("@","="))
+        frootcert.close()
+        fcert = open("sslcert.txt","w")
+        fcert.write(fcert_content.replace("@","="))
+        fcert.close()
+        fkey = open("sslkey.txt","w")
+        fkey.write(fkey_content.replace("@","="))
+        fkey.close()
+    
+        #Change unix permissions to restricted
+        os.chmod("sslrootcert.txt",0o600)
+        os.chmod("sslcert.txt",0o600)
+        os.chmod("sslkey.txt",0o600)
+    
+        sslmode="sslmode=verify-ca"
+        sslrootcert = "sslrootcert=sslrootcert.txt"
+        sslcert = "sslcert=sslcert.txt"
+        sslkey = "sslkey=sslkey.txt"
+        hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
+        user = "user=postgres"
+        password = "password={}".format(os.environ.get('PG_PASSWORD'))
+        dbname="dbname=mgmt590"
+    
+        dbconnect = " ".join([sslmode,
+                                sslrootcert,
+                                sslcert,
+                                sslkey,
+                                hostaddr,
+                                user,
+                                dbname,password])
+        con = psycopg2.connect(dbconnect)
+        cur = con.cursor()
+        out = []
+        for row in cur.execute(sql_rev):
+            out.append({
+                "question": row[0],
+                "context": row[1],
+                "answer": row[2],
+                "model": row[3],
+                "timestamp": row[4]
+            })
+        con.close()
+
+        return jsonify(out)
+
+
+    # List models currently available for inference
+    @app.route("/models", methods=['GET'])
+    def list_model():
+
+        # Get the loaded models
+        models_loaded = []
+        for m in models['models']:
+            models_loaded.append({
+                'name': m['name'],
+                'tokenizer': m['tokenizer'],
+                'model': m['model']
+            })
+
+        return jsonify(models_loaded)
+
+
+    # Add a model to the models available for inference
+    @app.route("/models", methods=['PUT'])
+    def add_model():
+
+        # Get the request body data
+        data = request.json
+        
+        # Load the provided model
+        if not validate_model(data['name']):
+            models_rev = []
+            for m in models['models']:
+                models_rev.append(m)
+            models_rev.append({
+                    'name': data['name'],
+                    'tokenizer': data['tokenizer'],
+                    'model': data['model'],
+                    'pipeline': pipeline('question-answering', 
+                        model=data['model'], 
+                        tokenizer=data['tokenizer'])
+            })
+            models['models'] = models_rev
+
+        # Get the loaded models
+        models_loaded = []
+        for m in models['models']:
+            models_loaded.append({
+                'name': m['name'],
+                'tokenizer': m['tokenizer'],
+                'model': m['model']
+            })
+
+        return jsonify(models_loaded)
+
+
+    # Delete a model from the models available for inference
+    @app.route("/models", methods=['DELETE'])
+    def delete_model():
+
+        # Validate model name if given
+        if request.args.get('model') == None:
+            return "Model name not provided in query string", 400
+
+        # Error if trying to delete default model
+        if request.args.get('model') == models['default']:
+            return "Can't delete default model", 400
+
+        # Load the provided model
         models_rev = []
         for m in models['models']:
-            models_rev.append(m)
-        models_rev.append({
-                'name': data['name'],
-                'tokenizer': data['tokenizer'],
-                'model': data['model'],
-                'pipeline': pipeline('question-answering', 
-                    model=data['model'], 
-                    tokenizer=data['tokenizer'])
-        })
+            if m['name'] != request.args.get('model'):
+                models_rev.append(m)
         models['models'] = models_rev
 
-    # Get the loaded models
-    models_loaded = []
-    for m in models['models']:
-        models_loaded.append({
-            'name': m['name'],
-            'tokenizer': m['tokenizer'],
-            'model': m['model']
-        })
+        # Get the loaded models
+        models_loaded = []
+        for m in models['models']:
+            models_loaded.append({
+                'name': m['name'],
+                'tokenizer': m['tokenizer'],
+                'model': m['model']
+            })
 
-    return jsonify(models_loaded)
+        return jsonify(models_loaded)
 
-
-# Delete a model from the models available for inference
-@app.route("/models", methods=['DELETE'])
-def delete_model():
-
-    # Validate model name if given
-    if request.args.get('model') == None:
-        return "Model name not provided in query string", 400
-
-    # Error if trying to delete default model
-    if request.args.get('model') == models['default']:
-        return "Can't delete default model", 400
-
-    # Load the provided model
-    models_rev = []
-    for m in models['models']:
-        if m['name'] != request.args.get('model'):
-            models_rev.append(m)
-    models['models'] = models_rev
-
-    # Get the loaded models
-    models_loaded = []
-    for m in models['models']:
-        models_loaded.append({
-            'name': m['name'],
-            'tokenizer': m['tokenizer'],
-            'model': m['model']
-        })
-
-    return jsonify(models_loaded)
-
-
+    return app
 
 #--------------#
 #  FUNCTIONS   #
@@ -305,6 +316,9 @@ def answer_question(model_name, question, context):
 # Run main by default if running "python answer.py"
 if __name__ == '__main__':
 
+    # Create our Flask App
+    app = create_app()
+
     # Initialize our default model.
     models = { 
         "default": "distilled-bert",
@@ -322,11 +336,11 @@ if __name__ == '__main__':
 
     # Database setup
     # con = sqlite3.connect(db)
-    rootcert_content = format(os.environ.get('SERVER_CA'))
-    fcert_content = format(os.environ.get('CLIENT_CERT'))
-    fkey_content = format(os.environ.get('CLIENT_KEY'))
-      
-    #Write SSL content into temp files
+    rootcert_content = format(os.environ.get('PG_SSLROOTCERT'))
+    fcert_content = format(os.environ.get('PG_SSLCERT'))
+    fkey_content = format(os.environ.get('PG_SSLKEY'))
+          
+        #Write SSL content into temp files
     frootcert = open("sslrootcert.txt","w")
     frootcert.write(rootcert_content.replace("@","="))
     frootcert.close()
@@ -336,12 +350,12 @@ if __name__ == '__main__':
     fkey = open("sslkey.txt","w")
     fkey.write(fkey_content.replace("@","="))
     fkey.close()
-
-    #Change unix permissions to restricted
+    
+        #Change unix permissions to restricted
     os.chmod("sslrootcert.txt",0o600)
     os.chmod("sslcert.txt",0o600)
     os.chmod("sslkey.txt",0o600)
-
+    
     sslmode="sslmode=verify-ca"
     sslrootcert = "sslrootcert=sslrootcert.txt"
     sslcert = "sslcert=sslcert.txt"
@@ -349,15 +363,15 @@ if __name__ == '__main__':
     hostaddr = "hostaddr={}".format(os.environ.get('PG_HOST'))
     user = "user=postgres"
     password = "password={}".format(os.environ.get('PG_PASSWORD'))
-    dbname="dbname=mgmt590-database"
-
+    dbname="dbname=mgmt590"
+    
     dbconnect = " ".join([sslmode,
-                            sslrootcert,
-                            sslcert,
-                            sslkey,
-                            hostaddr,
-                            user,
-                            dbname,password])
+                                sslrootcert,
+                                sslcert,
+                                sslkey,
+                                hostaddr,
+                                user,
+                                dbname,password])
     con = psycopg2.connect(dbconnect)
     cur = con.cursor()
     cur.execute('''CREATE TABLE answers
